6933819bc22dcc96e6faab3759c0031801beb360
Joseph T. Lapp <arachnojoe@gmail.com>
2023-01-22 13:50:57 -0500

    FieldsOf treats single underscore as private

--- libs/utilities/src/lib/fields_of.ts
+++ libs/utilities/src/lib/fields_of.ts
@@ -1,7 +1,7 @@
 /**
- * Matches object properties not beginning with a double underscore.
+ * Matches object properties beginning with neither `_` nor `#`.
  */
-export type PublicProperty<P> = P extends `__${string}` ? never : P;
+export type PublicProperty<P> = P extends `_${string}` ? never : P;
 
 /**
  * Matches objects that are functions.
@@ -10,15 +10,15 @@ export type IsFunction<T> = T extends (...args: any[]) => any ? T : never;
 
 /**
  * Type consisting of all public non-function properties of T, where
- * public properties are those neither beginning with `#` nor `__`.
+ * public properties are those beginning with neither `#` nor `_`.
  */
 export type FieldsOf<T> = Pick<
   T,
   {
     [K in keyof T]: K extends PublicProperty<K>
-    ? T[K] extends IsFunction<T[K]>
-    ? never
-    : K
-    : never;
+      ? T[K] extends IsFunction<T[K]>
+        ? never
+        : K
+      : never;
   }[keyof T]
 >;
