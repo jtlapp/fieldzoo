d44f556e2cda9a962db3ccf37f9988afe0070275
Joseph T. Lapp <arachnojoe@gmail.com>
2023-01-24 21:03:17 -0500

    feat: add single and multi line unicode regexes

--- libs/utilities/src/lib/regexes.test.ts
+++ libs/utilities/src/lib/regexes.test.ts
@@ -1,4 +1,58 @@
-import { USER_NAME_REGEX } from "./regexes";
+import {
+  MULTI_LINE_UNICODE_REGEX,
+  SINGLE_LINE_UNICODE_REGEX,
+  USER_NAME_REGEX,
+} from "./regexes";
+
+describe("SINGLE_LINE_UNICODE_REGEX", () => {
+  const regex = SINGLE_LINE_UNICODE_REGEX;
+
+  it("accepts valid text", () => {
+    expect(matches("M", regex)).toBe(true);
+    expect(matches("Jimmy", regex)).toBe(true);
+    expect(matches("Mary Q.", regex)).toBe(true);
+    expect(matches("abc d e fgh*32 + 1 <25> [!]'\" {,}", regex)).toBe(true);
+    expect(matches("Mark Heüße", regex)).toBe(true);
+  });
+
+  it("rejects invalid text", () => {
+    expect(matches("", regex)).toBe(false);
+    expect(matches("\n", regex)).toBe(false);
+    expect(matches("  ", regex)).toBe(false);
+    expect(matches("X  Y", regex)).toBe(false);
+    expect(matches(" Xy", regex)).toBe(false);
+    expect(matches("Xy ", regex)).toBe(false);
+    expect(matches("X\n", regex)).toBe(false);
+    expect(matches("X\nY", regex)).toBe(false);
+    expect(matches("X\tY", regex)).toBe(false);
+  });
+});
+
+describe("MULTI_LINE_UNICODE_REGEX", () => {
+  const regex = MULTI_LINE_UNICODE_REGEX;
+
+  it("accepts valid text", () => {
+    expect(matches("M", regex)).toBe(true);
+    expect(matches("Jimmy", regex)).toBe(true);
+    expect(matches("Mary Q.", regex)).toBe(true);
+    expect(matches("abc d e fgh*32 + 1 <25> [!]'\" {,}", regex)).toBe(true);
+    expect(matches("Mark Heüße", regex)).toBe(true);
+    expect(matches("X  Y", regex)).toBe(true);
+    expect(matches("X\nY", regex)).toBe(true);
+    expect(matches("X \n Y", regex)).toBe(true);
+    expect(matches("X\n\nY\nZ", regex)).toBe(true);
+  });
+
+  it("rejects invalid text", () => {
+    expect(matches("", regex)).toBe(false);
+    expect(matches("\n", regex)).toBe(false);
+    expect(matches("  ", regex)).toBe(false);
+    expect(matches(" Xy", regex)).toBe(false);
+    expect(matches("Xy ", regex)).toBe(false);
+    expect(matches("X\n", regex)).toBe(false);
+    expect(matches("X\tY", regex)).toBe(false);
+  });
+});
 
 describe("USER_NAME_REGEX", () => {
   const regex = USER_NAME_REGEX;
@@ -20,12 +74,15 @@ describe("USER_NAME_REGEX", () => {
 
   it("rejects invalid user names", () => {
     expect(matches("", regex)).toBe(false);
+    expect(matches(" M", regex)).toBe(false);
+    expect(matches("M ", regex)).toBe(false);
     expect(matches("M.", regex)).toBe(false);
     expect(matches("X..Y", regex)).toBe(false);
     expect(matches("X  Y", regex)).toBe(false);
     expect(matches("X--Y", regex)).toBe(false);
     expect(matches(". Foo", regex)).toBe(false);
     expect(matches("-Foo", regex)).toBe(false);
+    expect(matches("Mooo ", regex)).toBe(false);
     expect(matches("'Foo", regex)).toBe(false);
     expect(matches("Mike 'Brien", regex)).toBe(false);
     expect(matches("foo@abc.com", regex)).toBe(false);
--- libs/utilities/src/lib/regexes.ts
+++ libs/utilities/src/lib/regexes.ts
@@ -1,4 +1,17 @@
 /**
+ * Regex matching a one or more lines of Unicode, with first and last lines
+ * non-empty, permitting repeating spaces and line feeds ("\n").
+ */
+export const MULTI_LINE_UNICODE_REGEX =
+  /^[^\p{Z}\p{C}]+(?:[ \n]+[^\p{Z}\p{C}]+)*$/u;
+
+/**
+ * Regex matching a single non-empty line of Unicode, with no double-spaces.
+ */
+export const SINGLE_LINE_UNICODE_REGEX =
+  /^[^\p{Z}\p{C}]+(?: [^\p{Z}\p{C}]+)*$/u;
+
+/**
  * Regex matching strings suitable for non-handle user names.
  */
 export const USER_NAME_REGEX =
