1c116f4b92d8b890df5aaa8445122f4c9cc10484
Joseph T. Lapp <arachnojoe@gmail.com>
2023-02-02 20:56:52 -0500

    feat: share DB env var prefix

--- libs/config/src/lib/app-constants.ts
+++ libs/config/src/lib/app-constants.ts
@@ -6,3 +6,8 @@
  * Prefix for environment variables used by this application.
  */
 export const ENVVAR_PREFIX = "FIELDZOO_";
+
+/**
+ * Prefix for database environment variables.
+ */
+export const DB_ENVVAR_PREFIX = ENVVAR_PREFIX + "DB_";
