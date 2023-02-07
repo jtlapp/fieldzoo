86578adb79d71d0d26c977cd3dc007244fa622e6
Joseph T. Lapp <arachnojoe@gmail.com>
2023-02-06 21:40:31 -0500

    fix: correct use of commander global options

--- apps/installer/src/app/installer.ts
+++ apps/installer/src/app/installer.ts
@@ -23,9 +23,10 @@ import { MIGRATION_FILE_PATH } from "@fieldzoo/database";
 
 import { dropAllTables, existingTables } from "@fieldzoo/utilities";
 
-interface CommandOptions {
-  env: string;
-}
+const PATH_TO_ROOT = path.join(__dirname, "../../../..");
+
+/** Root-relative path name of .env file having DB credentials */
+let envFileName: string; // has a default value, so never null
 
 class CommandFailure extends Error {
   constructor(message: string) {
@@ -36,49 +37,49 @@ class CommandFailure extends Error {
 program
   .name("installer")
   .description(
-    "Field Zoo Installer. Installs or upgrades the Field Zoo database tables, connecting to the database using the information in the specified .env file."
+    "Field Zoo Installer. Installs or upgrades the Field Zoo database tables, connecting to the database using the information in the specified .env file.",
   )
   .option("--env <path>", "Path to and including the .env file", ".env-admin")
+  .on("option:env", (value) => (envFileName = value))
   .addHelpText(
     "after",
     `Environment variables (.env):\n${Object.entries(
-      DatabaseConfig.getHelpInfo(DB_ENVVAR_PREFIX)
+      DatabaseConfig.getHelpInfo(DB_ENVVAR_PREFIX),
     )
       .map((entry) => `  ${entry[0]} - ${entry[1]}`)
-      .join("\n")}`
+      .join("\n")}`,
   );
 
 program
   .command("install", { isDefault: true })
   .description(
-    "Install the database tables from scratch, but only if it doesn't already exist (default command when none given)"
+    "Install the database tables from scratch, but only if it doesn't already exist (default command when none given)",
   )
-  .action(commandWrapper.bind(doInstall));
+  .action(commandWrapper.bind(null, doInstall));
 
 program
   .command("reinstall")
   .description(
-    "Drop the database tables and then installs them from scratch, if tables already exist in the database"
+    "Drop the database tables and then installs them from scratch, if tables already exist in the database",
   )
-  .action(commandWrapper.bind(doReinstall));
+  .action(commandWrapper.bind(null, doReinstall));
 
 program
   .command("upgrade")
   .description("Update existing database tables to the latest schema")
-  .action(commandWrapper.bind(doUpgrade));
+  .action(commandWrapper.bind(null, doUpgrade));
 
 program.parse();
 
 async function commandWrapper(
   action: (db: Kysely<any>) => Promise<void>,
-  options: CommandOptions
 ): Promise<void> {
   let db: Kysely<any> | null = null;
   try {
     try {
-      dotenv.config({ path: path.join(process.cwd(), options.env) });
+      dotenv.config({ path: path.join(PATH_TO_ROOT, envFileName) });
     } catch (err: any) {
-      throw new CommandFailure(`File '${options.env}' not found`);
+      throw new CommandFailure(`File '${envFileName}' not found`);
     }
     db = new Kysely<any>({
       dialect: new PostgresDialect({
@@ -103,7 +104,7 @@ async function doInstall(db: Kysely<any>) {
   const tableNames = await existingTables(db);
   if (tableNames.length > 0) {
     throw new CommandFailure(
-      "The database already contains tables. Use the 'reinstall' command to delete these tables before installing."
+      "The database already contains tables. Use the 'reinstall' command to delete these tables before installing.",
     );
   }
   if (!(await migrateToLatestSchema(db))) {
@@ -115,7 +116,7 @@ async function doReinstall(db: Kysely<any>) {
   const tableNames = await existingTables(db);
   if (tableNames.length == 0) {
     throw new CommandFailure(
-      "The database does not contain any tables. Use the 'install' command to install the tables."
+      "The database does not contain any tables. Use the 'install' command to install the tables.",
     );
   }
   await dropAllTables(db);
@@ -128,7 +129,7 @@ async function doUpgrade(db: Kysely<any>) {
   const tableNames = await existingTables(db);
   if (tableNames.length == 0) {
     throw new CommandFailure(
-      "The database does not contain any tables. Use the 'install' command to install the tables."
+      "The database does not contain any tables. Use the 'install' command to install the tables.",
     );
   }
   if (!(await migrateToLatestSchema(db))) {
@@ -152,7 +153,7 @@ async function migrateToLatestSchema(db: Kysely<any>): Promise<boolean> {
     console.log(
       `${result.status == "Success" ? "success" : "failed"}: migration ${
         result.migrationName
-      }`
+      }`,
     );
   });
   console.error(error);
