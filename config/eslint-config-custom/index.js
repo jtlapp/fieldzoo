378843ae2f2d97496d844a4747e1a8204400c3b4
Joseph T. Lapp <arachnojoe@gmail.com>
2023-02-17 16:46:38 -0500

    chore: comment out eslint rules so I can relearn eslint dependency

--- config/eslint-config-custom/index.js
+++ config/eslint-config-custom/index.js
@@ -1,10 +1,10 @@
 module.exports = {
   extends: ["next", "turbo", "prettier"],
   rules: {
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-unused-vars": "off",
+    // "@typescript-eslint/no-empty-function": "off",
+    // "@typescript-eslint/no-explicit-any": "off",
+    // "@typescript-eslint/no-non-null-assertion": "off",
+    // "@typescript-eslint/no-unused-vars": "off",
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
