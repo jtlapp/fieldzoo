6331c581e5b7aa0dfb2bbd6cd3b6edc7ab33ae5a
Joseph T. Lapp <arachnojoe@gmail.com>
2023-02-25 14:33:02 -0500

    chore: share eslint config via relative paths

---
+++ config/eslint-config-custom-react.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['next', 'turbo', 'prettier'],
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve('next/babel')],
+    },
+  },
+  settings: {
+    next: {
+      rootDir: ['apps/*/'],
+    },
+  },
+};
