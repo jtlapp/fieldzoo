3d72b0b7e5019e77c1c0dc7c2ccea628996f574a
Joseph T. Lapp <arachnojoe@gmail.com>
2023-03-04 21:14:17 -0500

    test: move tests of KyselyFacet to its own file

---
+++ utils/kysely-facets/src/facets/KyselyFacet.test.ts
@@ -0,0 +1,57 @@
+import { Kysely } from "kysely";
+
+import { createDB, resetDB, destroyDB } from "../test-utils/test-setup";
+import { Database, UserTable } from "../test-utils/test-tables";
+import { USERS } from "../test-utils/test-objects";
+
+let db: Kysely<Database>;
+let userTable: UserTable;
+
+beforeAll(async () => {
+  db = await createDB();
+  userTable = new UserTable(db);
+});
+beforeEach(() => resetDB(db));
+afterAll(() => destroyDB(db));
+
+describe("basic row queries", () => {
+  it("inserts, selects, updates, and deletes objects by row query", async () => {
+    // Add users by row query
+    const user0 = (await userTable
+      .insertRows()
+      .values(USERS[0])
+      .returningAll()
+      .executeTakeFirst())!;
+    const user1 = (await userTable
+      .insertRows()
+      .values(USERS[1])
+      .returningAll()
+      .executeTakeFirst())!;
+
+    // Update a user by row query
+    const NEW_EMAIL = "new@baz.com";
+    user1.email = NEW_EMAIL;
+    await userTable
+      .updateRows()
+      .set(user1)
+      .where("id", "=", user1.id)
+      .execute();
+
+    // Retrieves user by row query
+    const readUser1 = await userTable
+      .selectRows()
+      .where("id", "=", user1.id)
+      .executeTakeFirst();
+    expect(readUser1?.handle).toEqual(USERS[1].handle);
+    expect(readUser1?.email).toEqual(NEW_EMAIL);
+
+    // Delete user by row query
+    await userTable.deleteRows().where("id", "=", user1.id).execute();
+
+    // Verify correct user was deleted
+    const readUser0 = await userTable.selectById(user0.id);
+    expect(readUser0?.handle).toEqual(USERS[0].handle);
+    const noUser = await userTable.selectById(user1.id);
+    expect(noUser).toBeNull();
+  });
+});
--- utils/kysely-facets/src/facets/StandardFacet.test.ts
+++ utils/kysely-facets/src/facets/StandardFacet.test.ts
@@ -17,48 +17,6 @@ beforeAll(async () => {
 beforeEach(() => resetDB(db));
 afterAll(() => destroyDB(db));
 
-describe("row queries", () => {
-  it("inserts, selects, updates, and deletes objects by row query", async () => {
-    // Add users by row query
-    const user0 = (await userTable
-      .insertRows()
-      .values(USERS[0])
-      .returningAll()
-      .executeTakeFirst())!;
-    const user1 = (await userTable
-      .insertRows()
-      .values(USERS[1])
-      .returningAll()
-      .executeTakeFirst())!;
-
-    // Update a user by row query
-    const NEW_EMAIL = "new@baz.com";
-    user1.email = NEW_EMAIL;
-    await userTable
-      .updateRows()
-      .set(user1)
-      .where("id", "=", user1.id)
-      .execute();
-
-    // Retrieves user by row query
-    const readUser1 = await userTable
-      .selectRows()
-      .where("id", "=", user1.id)
-      .executeTakeFirst();
-    expect(readUser1?.handle).toEqual(USERS[1].handle);
-    expect(readUser1?.email).toEqual(NEW_EMAIL);
-
-    // Delete user by row query
-    await userTable.deleteRows().where("id", "=", user1.id).execute();
-
-    // Verify correct user was deleted
-    const readUser0 = await userTable.selectById(user0.id);
-    expect(readUser0?.handle).toEqual(USERS[0].handle);
-    const noUser = await userTable.selectById(user1.id);
-    expect(noUser).toBeNull();
-  });
-});
-
 describe("insertMany()", () => {
   it("inserts rows without returning columns", async () => {
     const result = await userTable.insertMany(USERS);
