eab66c2ccf40437bc05bd15d1e8404495da1b53c
Joseph T. Lapp <arachnojoe@gmail.com>
2023-03-09 13:00:37 -0500

    refactor: split KyselyFacet tests into rows and transform files

--- utils/kysely-facets/src/tests/KyselyFacet.transform.test.ts
+++ utils/kysely-facets/src/tests/KyselyFacet.transform.test.ts
@@ -4,7 +4,6 @@ import { KyselyFacet } from "../..";
 import { createDB, resetDB, destroyDB } from "./utils/test-setup";
 import { Database, Users } from "./utils/test-tables";
 import {
-  USERS,
   userRow1,
   userRow2,
   selectedUser1,
@@ -24,63 +23,12 @@ export class PlainUserFacet extends KyselyFacet<
 }
 
 let db: Kysely<Database>;
-let plainUserFacet: PlainUserFacet;
 
 beforeAll(async () => {
   db = await createDB();
-  plainUserFacet = new PlainUserFacet(db);
 });
 beforeEach(() => resetDB(db));
 afterAll(() => destroyDB(db));
-
-describe("basic row queries", () => {
-  it("inserts, selects, updates, and deletes objects by row query", async () => {
-    // Add users by row query
-    const user0 = (await plainUserFacet
-      .insertRows()
-      .values(USERS[0])
-      .returningAll()
-      .executeTakeFirst())!;
-    const user1 = (await plainUserFacet
-      .insertRows()
-      .values(USERS[1])
-      .returningAll()
-      .executeTakeFirst())!;
-
-    // Update a user by row query
-    const NEW_EMAIL = "new@baz.com";
-    user1.email = NEW_EMAIL;
-    await plainUserFacet
-      .updateRows()
-      .set(user1)
-      .where("id", "=", user1.id)
-      .execute();
-
-    // Retrieves user by row query
-    const readUser1 = await plainUserFacet
-      .selectRows()
-      .where("id", "=", user1.id)
-      .executeTakeFirst();
-    expect(readUser1?.handle).toEqual(USERS[1].handle);
-    expect(readUser1?.email).toEqual(NEW_EMAIL);
-
-    // Delete user by row query
-    await plainUserFacet.deleteRows().where("id", "=", user1.id).execute();
-
-    // Verify correct user was deleted
-    const readUser0 = await plainUserFacet
-      .selectRows()
-      .where("id", "=", user0.id)
-      .executeTakeFirst();
-    expect(readUser0?.handle).toEqual(USERS[0].handle);
-
-    const noUser = await plainUserFacet
-      .selectRows()
-      .where("id", "=", user1.id)
-      .executeTakeFirst();
-    expect(noUser).toBeUndefined();
-  });
-});
 
 describe("transforms selection objects", () => {
   class TestPassThruFacet extends KyselyFacet<Database, "users"> {
