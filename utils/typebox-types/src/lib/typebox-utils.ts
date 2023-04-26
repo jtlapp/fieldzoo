b96aadd3228d42d1e6b0806bf20a52dcf2aec69f
Joseph T. Lapp <arachnojoe@gmail.com>
2023-04-26 09:03:18 -0400

    feat: add EmptyStringable

--- utils/typebox-types/src/lib/typebox-utils.ts
+++ utils/typebox-types/src/lib/typebox-utils.ts
@@ -3,6 +3,9 @@ import { StringOptions, TSchema, Type } from "@sinclair/typebox";
 export const NonEmptyString = (options?: StringOptions<string>) =>
   Type.String({ minLength: 1, ...options });
 
+export const EmptyStringable = <T extends TSchema>(type: T) =>
+  Type.Union([type, Type.Literal("")]);
+
 export const Nullable = <T extends TSchema>(type: T) =>
   Type.Union([type, Type.Null()]);
 
