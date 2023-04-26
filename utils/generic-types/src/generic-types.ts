bb954df7072df3bbccfd95e0021d89a5ac01dce5
Joseph T. Lapp <arachnojoe@gmail.com>
2023-04-25 21:38:46 -0400

    refactor: rename FieldsOf to Fields; add Unvalidated/UnvalidatedFields

--- utils/generic-types/src/generic-types.ts
+++ utils/generic-types/src/generic-types.ts
@@ -7,7 +7,7 @@ export type ClassType<T> = { new (): T };
  * Type consisting of all public non-function properties of T, where
  * public properties are those beginning with neither `#` nor `_`.
  */
-export type FieldsOf<T> = Pick<
+export type Fields<T> = Pick<
   T,
   {
     [K in keyof T]: K extends PublicProperty<K>
@@ -24,12 +24,25 @@ export type FieldsOf<T> = Pick<
 export type IsFunction<T> = T extends (...args: any[]) => any ? T : never;
 
 /**
- * Make selective properties of an object optional.
+ * Matches object properties beginning with neither `_` nor `#`.
+ */
+export type PublicProperty<P> = P extends `_${string}` ? never : P;
+
+/**
+ * Makes selective properties of an object optional.
  */
 export type SelectivePartial<T, K extends keyof T> = Omit<T, K> &
   Partial<Pick<T, K>>;
 
 /**
- * Matches object properties beginning with neither `_` nor `#`.
+ * Removes the `__validated__` symbol from a type, if present.
  */
-export type PublicProperty<P> = P extends `_${string}` ? never : P;
+export type Unvalidated<V> = V extends object ? Omit<V, "__validated__"> : V;
+
+/**
+ * Removes the `__validated__` symbol from all public non-function
+ * properties of a type.
+ */
+export type UnvalidatedFields<O> = {
+  [K in keyof Fields<O>]: Unvalidated<O[K]>;
+};
