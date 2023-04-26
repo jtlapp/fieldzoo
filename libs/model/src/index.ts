795d9a6706cfdd0662066ad2b189ff64a6dec594
Joseph T. Lapp <arachnojoe@gmail.com>
2023-04-26 09:17:25 -0400

    feat: export new values

--- libs/model/src/index.ts
+++ libs/model/src/index.ts
@@ -1,3 +1,12 @@
 export * from "./entities/glossary";
 export * from "./entities/term";
 export * from "./entities/user";
+
+export * from "./values/display-name";
+export * from "./values/email-address";
+export * from "./values/glossary-id";
+export * from "./values/multiline-description";
+export * from "./values/normalized-name";
+export * from "./values/term-id";
+export * from "./values/user-id";
+export * from "./values/user-name";
