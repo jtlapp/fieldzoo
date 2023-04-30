d2447fbb7b49b7df9c486db2af9b7ebd44767413
Joseph T. Lapp <arachnojoe@gmail.com>
2023-04-29 20:21:09 -0400

    refactor: rename modeling/src/migrations to modeling/src/tables

--- utils/modeling/src/index.ts
+++ utils/modeling/src/index.ts
@@ -1,4 +1,4 @@
 export * from "./entities/timestamped-entity";
 export * from "./entities/timestamped-entity.util";
-export * from "./migration/timestamped-table";
+export * from "./tables/timestamped-table";
 export * from "./repos/timestamped-repo";
