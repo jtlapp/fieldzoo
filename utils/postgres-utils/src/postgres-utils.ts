8dcd1059dde5c37fbacd00895a549064a35ea0f1
Joseph T. Lapp <arachnojoe@gmail.com>
2023-05-11 21:25:23 -0400

    feat/refactor!: rename dropAllTables() to clearDatabase(), dropping all functions too

--- utils/postgres-utils/src/postgres-utils.ts
+++ utils/postgres-utils/src/postgres-utils.ts
@@ -10,9 +10,9 @@ interface Schema {
 }
 
 /**
- * Drops all tables in the current database.
+ * Drops everything from the current database.
  */
-export async function dropAllTables(db: Kysely<Schema>): Promise<void> {
+export async function clearDatabase(db: Kysely<Schema>): Promise<void> {
   const tableNames = await existingTables(db);
   if (tableNames.length > 0) {
     // It is necessary to quote the table name to keep Postgres from
@@ -22,12 +22,36 @@ export async function dropAllTables(db: Kysely<Schema>): Promise<void> {
       .join("\n")}`;
     await sql.raw(dropSql).execute(db);
   }
+
+  const functionNames = await existingFunctions(db);
+  if (functionNames.length > 0) {
+    const dropSql = `${functionNames
+      .map(
+        (functionName) => `drop function if exists "${functionName}" cascade;`
+      )
+      .join("\n")}`;
+    await sql.raw(dropSql).execute(db);
+  }
+}
+
+/**
+ * Returns the names of all existing functions in the current database.
+ */
+export async function existingFunctions(db: Kysely<Schema>): Promise<string[]> {
+  const result = await sql
+    .raw<{ name: string }>(
+      `select routine_name as name from information_schema.routines
+        where routine_type = 'FUNCTION' and routine_schema in ('public', 'auth')`
+    )
+    .execute(db);
+  return result.rows.map((row) => row.name);
 }
 
 /**
  * Returns the names of all tables in the current database.
  */
 export async function existingTables(db: Kysely<Schema>): Promise<string[]> {
+  // TODO: rewrite as raw sql to be able to discard types
   const rows = await db
     .selectFrom("pg_tables")
     .select("tablename")
