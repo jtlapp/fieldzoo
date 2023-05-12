dea81ec6fc4614a6adb2368f5b4d991845579e50
Joseph T. Lapp <arachnojoe@gmail.com>
2023-05-11 21:26:30 -0400

    refactor!: move modeling test utils to separate import to fix installer import issue

--- utils/modeling/src/index.ts
+++ utils/modeling/src/index.ts
@@ -3,13 +3,5 @@ export * from "./tables/timestamped-table";
 export * from "./entities/timestamped-entity";
 
 export * from "./values/email-address";
-export * from "./values/email-address.test";
 export * from "./values/password-hash";
-export * from "./values/password-hash.test";
 export * from "./values/password-salt";
-export * from "./values/password-salt.test";
-
-export * from "./testing/value-test-consts";
-export * from "./testing/test-numeric-id";
-export * from "./testing/test-timestamp";
-export * from "./testing/test-values";
---
+++ utils/modeling/src/testing/index.ts
@@ -0,0 +1,8 @@
+export * from "../values/email-address.test";
+export * from "../values/password-hash.test";
+export * from "../values/password-salt.test";
+
+export * from "../testing/value-test-consts";
+export * from "../testing/test-numeric-id";
+export * from "../testing/test-timestamp";
+export * from "../testing/test-values";
