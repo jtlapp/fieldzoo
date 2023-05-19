fde35af8d7526e203a2ea4908330d1e50bbe1097
Joseph T. Lapp <arachnojoe@gmail.com>
2023-05-19 09:33:30 -0400

    chore: consolidate serially run tests in bin/nx-test.js

---
+++ bin/nx-test.js
@@ -0,0 +1,19 @@
+const child_process = require("child_process");
+
+const testsToRunSerially = [
+  "@fieldzoo/system-model",
+  "@fieldzoo/taxonomic-model",
+  "installer",
+];
+
+const args = process.argv.slice(2);
+try {
+  child_process.execSync(
+    `nx run-many --t test ${args.join(" ")} ${testsToRunSerially.join(",")}`,
+    {
+      stdio: "inherit",
+    }
+  );
+} catch (e) {
+  process.exit(1);
+}
