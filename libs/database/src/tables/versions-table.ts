d7c1456893566baf8484e113979d3cf8a5883b5c
Joseph T. Lapp <arachnojoe@gmail.com>
2023-06-07 07:27:54 -0400

    refactor: move versions table creator to versions-table file

---
+++ libs/database/src/tables/versions-table.ts
@@ -0,0 +1,30 @@
+import { CreateTableBuilder, Kysely } from "kysely";
+import { CollaborativeColumns } from "../tables/collaborative-table";
+
+/**
+ * Utility class for creating version tables. Not based on
+ * `TimestampedTable` because it holds archived copies of rows
+ * from `TimestampedTable`-based tables. For example, `modifiedAt`
+ * does not update on trigger because the table does not update.
+ */
+export class VersionsTable {
+  static async create(
+    db: Kysely<any>,
+    tableName: string,
+    factory: (
+      tb: CreateTableBuilder<string, CollaborativeColumns>
+    ) => CreateTableBuilder<string, any>
+  ) {
+    await factory(
+      db.schema
+        .createTable(tableName)
+        .addColumn("createdAt", "timestamp", (col) => col.notNull())
+        .addColumn("modifiedAt", "timestamp", (col) => col.notNull())
+        .addColumn("modifiedBy", "uuid", (col) =>
+          col.references("user_profiles.id").notNull()
+        )
+        .addColumn("versionNumber", "integer", (col) => col.notNull())
+        .addColumn("whatChangedLine", "text", (col) => col.notNull())
+    ).execute();
+  }
+}
--- libs/database/src/utils/migration-utils.ts
+++ libs/database/src/utils/migration-utils.ts
@@ -3,30 +3,8 @@
  */
 
 import * as path from "path";
-import { CreateTableBuilder, Kysely } from "kysely";
-import { CollaborativeColumns } from "../tables/collaborative-table";
 
 /**
  * Returns the path to the migration files.
  */
 export const MIGRATION_FILE_PATH = path.join(__dirname, "../migrations");
-
-export async function createVersionsTable(
-  db: Kysely<any>,
-  tableName: string,
-  factory: (
-    tb: CreateTableBuilder<string, CollaborativeColumns>
-  ) => CreateTableBuilder<string, any>
-) {
-  await factory(
-    db.schema
-      .createTable(tableName)
-      .addColumn("createdAt", "timestamp", (col) => col.notNull())
-      .addColumn("modifiedAt", "timestamp", (col) => col.notNull())
-      .addColumn("modifiedBy", "uuid", (col) =>
-        col.references("user_profiles.id").notNull()
-      )
-      .addColumn("versionNumber", "integer", (col) => col.notNull())
-      .addColumn("whatChangedLine", "text", (col) => col.notNull())
-  ).execute();
-}
