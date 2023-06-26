bad72f136c1dce906d933efa8b04193ed573596b
Joseph T. Lapp <arachnojoe@gmail.com>
2023-06-26 16:21:39 -0400

    refactor: remove daisyui

--- config/tailwind-config/tailwind.config.js
+++ config/tailwind-config/tailwind.config.js
@@ -6,8 +6,5 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["light", "dark"],
-  },
+  plugins: [],
 };
