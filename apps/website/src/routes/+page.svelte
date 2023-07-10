4fd79614fca9b46ccda45e1735ed3379fad0b18e
Joseph T. Lapp <arachnojoe@gmail.com>
2023-07-10 16:54:05 -0400

    refactor!: install svelte-kit sharing config with monorepo

---
+++ apps/website/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { DummyComponent } from "ui";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<DummyComponent />
---
+++ libs/ui/src/lib/DummyComponent.svelte
@@ -0,0 +1,1 @@
+<div>Dummy Component</div>
