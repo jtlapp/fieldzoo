8f38c6dbfae99e805c69b92bf26f6a5452e7e87b
Joseph T. Lapp <arachnojoe@gmail.com>
2023-07-12 22:23:20 -0400

    refactor: remove unused code

--- apps/website/src/routes/DarkModeToggle.svelte
+++ apps/website/src/routes/DarkModeToggle.svelte
@@ -1,9 +1,5 @@
-<script context="module" lang="ts">
-  // const CONTRAST_ICON = `<svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g stroke-width="0"></g><g stroke-linecap="round" stroke-linejoin="round"></g><g><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g transform="translate(-180.000000, -4199.000000)" fill="#000000"><g transform="translate(56.000000, 160.000000)"><path d="M126,4049 C126,4044.589 129.589,4041 134,4041 L134,4057 C129.589,4057 126,4053.411 126,4049 M134,4039 C128.477,4039 124,4043.477 124,4049 C124,4054.523 128.477,4059 134,4059 C139.523,4059 144,4054.523 144,4049 C144,4043.477 139.523,4039 134,4039"></path></g></g></g></g></svg>`;
-</script>
-
 <script lang="ts">
-  // adapted from https://dev.to/willkre/persistent-theme-switch-dark-mode-with-svelte-sveltekit-tailwind-1b9g
+  // modified from https://dev.to/willkre/persistent-theme-switch-dark-mode-with-svelte-sveltekit-tailwind-1b9g
 
   import { browser } from "$app/environment";
 
