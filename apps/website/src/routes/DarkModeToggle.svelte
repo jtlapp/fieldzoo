82e4862fc5cf39de68b1fc23fba3e56a095ac8b8
Joseph T. Lapp <arachnojoe@gmail.com>
2023-07-15 20:43:06 -0400

    fix: config problems using melt in ui-components; demo fix with revised DarkModeToggle

--- apps/website/src/routes/DarkModeToggle.svelte
+++ apps/website/src/routes/DarkModeToggle.svelte
@@ -2,6 +2,7 @@
   // modified from https://dev.to/willkre/persistent-theme-switch-dark-mode-with-svelte-sveltekit-tailwind-1b9g
 
   import { browser } from "$app/environment";
+  import { DarkLightIcon } from "ui-components";
 
   let classes = "";
   export { classes as class };
@@ -40,29 +41,7 @@
     type="checkbox"
   />
   <label for="theme-toggle">
-    <svg
-      class="fill-foreground dark:fill-dark-foreground"
-      viewBox="0 0 20 20"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-      ><g stroke-width="0" /><g
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      /><g
-        ><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"
-          ><g
-            class="fill-foreground dark:fill-dark-foreground"
-            transform="translate(-180.000000, -4199.000000)"
-            ><g transform="translate(56.000000, 160.000000)"
-              ><path
-                d="M126,4049 C126,4044.589 129.589,4041 134,4041 L134,4057 C129.589,4057 126,4053.411 126,4049 M134,4039 C128.477,4039 124,4043.477 124,4049 C124,4054.523 128.477,4059 134,4059 C139.523,4059 144,4054.523 144,4049 C144,4043.477 139.523,4039 134,4039"
-              /></g
-            ></g
-          ></g
-        ></g
-      ></svg
-    >
+    <DarkLightIcon class="fill-foreground dark:fill-dark-foreground" />
   </label>
 </div>
 
--- libs/ui-components/src/index.ts
+++ libs/ui-components/src/index.ts
@@ -1,2 +1,6 @@
 export { default as Button } from "./lib/Button.svelte";
 export { default as DownCaret } from "./lib/DownCaret.svelte";
+
+export { default as CheckIcon } from "./icons/CheckIcon.svelte";
+export { default as ChevronRightIcon } from "./icons/ChevronRightIcon.svelte";
+export { default as DarkLightIcon } from "./icons/DarkLightIcon.svelte";
